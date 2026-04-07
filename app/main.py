40943cbb02af028c428f1779276cf6c8b8d8b924
Kali <manishpc@localhost>
2026-04-07 17:05:47 +0530

    readme.md update

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from .routers import poi
+from .database import Base, engine
+from . import models
+
+
+# Create all tables
+Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI(title="Digital City Explorer API")
 
 # Mount router with versioning
 app.include_router(poi.router, prefix="/api/v1")
+
